240f3ace8f9b97ff1e5272723573a9d5e84da372
Chris Klanac <cklanac@users.noreply.github.com>
2018-11-29 13:20:07 -0500

    chore: remove glitch files

--- populate.js
+++ populate.js
@@ -4,8 +4,8 @@ mongoose.Promise = global.Promise;
 
 var BookSchema = new mongoose.Schema({
   title: String,
-  reviews: [{ 
-    user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}, // 'aaaaaaaaaaaaaa'
+  reviews: [{
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     comments: String
   }]
 });
@@ -14,12 +14,12 @@ var Book = mongoose.model('Book', BookSchema);
 
 var UserSchema = new mongoose.Schema({
   username: {
-      type: String,
-      required: true
+    type: String,
+    required: true
   },
   fullName: {
-      type: String,
-      required: true
+    type: String,
+    required: true
   },
   addresses: [{
     street: String,
@@ -31,37 +31,57 @@ var UserSchema = new mongoose.Schema({
     zip: Number
   }]
 });
- 
+
 var User = mongoose.model('User', UserSchema);
 
-mongoose.connect(
-    process.env.DATABASE_URL, 
-    {useMongoClient: true}
-  ).then(() => {
-    mongoose.connection.db.dropDatabase()
-  }).then(() => {
+mongoose.connect('mongodb://localhost:27017/demo-books', { useMongoClient: true }
+).then(() => {
+  return mongoose.connection.db.dropDatabase();
+}).then(() => {
 
-  var bookPromise = Book.create({ 
-      title: 'Hitch Hikers Guide to the Galaxy',
-      reviews: [ 
-        {user:'aaaaaaaaaaaaaaaaaaaaaaaa', comments: 'And... Lorem ipsum dolor sit amet, consectetur adipiscing.'},
-        {user:'bbbbbbbbbbbbbbbbbbbbbbbb', comments: 'But... Sed ut perspiciatis unde omnis iste natus error.'},
-        {user:'cccccccccccccccccccccccc', comments: 'Can... At vero eos et accusamus et iusto odio dignissimos'}
-      ]
-    })
-  
-    var userPromise = User.create([
-      {_id: 'aaaaaaaaaaaaaaaaaaaaaaaa', username: 'aalabaster', fullName : 'Alice Alabaster',addresses: [{street: '123 Main St.',city: 'Springfield',state: 'CO',zip: 12345},{street: '789 Cactus St.',city: 'Scottsdale',state: 'AZ',zip: 56789}]},      
-      {_id: 'bbbbbbbbbbbbbbbbbbbbbbbb', username: 'bbaker', fullName : 'Bob Baker',addresses: [{street: '123 Front St.',city: 'Downtown',state: 'CA',zip: 12345}]},
-      {_id: 'cccccccccccccccccccccccc', username: 'cconway', fullName : 'Chuck Conway'}
-      ]
-    );  
-    return Promise.all([userPromise, bookPromise])
-  }).then(results => {
-    console.log('new', results);
-  }).then(() => {
-    return Book.find().populate({ path: 'reviews.user', select: 'fullName username' }); //.populate('reviews.user') 
-  }).then(results => {  
+  var bookPromise = Book.create({
+    title: 'Hitch Hikers Guide to the Galaxy',
+    reviews: [
+      { user: 'aaaaaaaaaaaaaaaaaaaaaaaa', comments: 'And... Lorem ipsum dolor sit amet, consectetur adipiscing.' },
+      { user: 'bbbbbbbbbbbbbbbbbbbbbbbb', comments: 'But... Sed ut perspiciatis unde omnis iste natus error.' },
+      { user: 'cccccccccccccccccccccccc', comments: 'Can... At vero eos et accusamus et iusto odio dignissimos' }
+    ]
+  });
+
+  var userPromise = User.create([
+    { _id: 'aaaaaaaaaaaaaaaaaaaaaaaa', username: 'aalabaster', fullName: 'Alice Alabaster', 
+      addresses: [
+        { street: '123 Main St.', city: 'Springfield', state: 'CO', zip: 12345 }, 
+        { street: '789 Cactus St.', city: 'Scottsdale', state: 'AZ', zip: 56789 }
+      ] },
+    { _id: 'bbbbbbbbbbbbbbbbbbbbbbbb', username: 'bbaker', fullName: 'Bob Baker', addresses: [{ street: '123 Front St.', city: 'Downtown', state: 'CA', zip: 12345 }] },
+    { _id: 'cccccccccccccccccccccccc', username: 'cconway', fullName: 'Chuck Conway' }
+  ]);
+
+
+  return Promise.all([userPromise, bookPromise]);
+
+
+}).then(results => {
+
+  console.log('new', results);
+
+})
+  .then(() => {
+    return Book.find()
+      // .populate('reviews.user', '-addresses -__v');
+      .populate({ path: 'reviews.user', select: 'fullName username' });
+
+  })
+  .then(results => {
     console.log('======', JSON.stringify(results, null, 4));
-  });   
-  
+
+    return mongoose.disconnect();
+  })
+  .catch((err) => {
+    console.error(err);
+    return mongoose.disconnect();
+  });
+
+
+
--- subdocument.js
+++ subdocument.js
@@ -1,20 +1,18 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
-
-var UserSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   username: {
-      type: String,
-      required: true
+    type: String,
+    required: true
   },
   fullName: {
-      type: String,
-      required: true
+    type: String,
+    required: true
   },
   addresses: [{
     street: String,
     city: String,
-    likes: {type: Number, default: 0},
+    likes: { type: Number, default: 0 },
     state: {
       type: String,
       enum: ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT']
@@ -22,30 +20,29 @@ var UserSchema = new mongoose.Schema({
     zip: Number
   }]
 });
- 
-var User = mongoose.model('User', UserSchema);
 
-mongoose.connect(
-    process.env.DATABASE_URL, 
-    {useMongoClient: true}
-  ).then(() => {
-    mongoose.connection.db.dropDatabase()
+const User = mongoose.model('User', UserSchema);
+
+mongoose.connect('mongodb://localhost:27017/demo-books', { useMongoClient: true })
+  .then(() => {
+    return mongoose.connection.db.dropDatabase();
   })
-  
-  
+
   // ***************************** 
   // Create a new User
   // *****************************   
   .then(() => {
 
-    return User.create({ 
-      _id: 'aaaaaaaaaaaaaaaaaaaaaaaa', 
-      username: 'aalabaster', fullName : 'Alice Alabaster',
-      addresses: [
-        {street: '123 Main St.',city: 'Springfield',state: 'CO',zip: 12345}
-      ]}
-    );  
-  
+    return User.create([
+      {
+        _id: 'aaaaaaaaaaaaaaaaaaaaaaaa', username: 'aalabaster', fullName: 'Alice Alabaster',
+        addresses: [
+          { street: '123 Main St.', city: 'Springfield', state: 'CO', zip: 12345 }]
+      },
+      { _id: 'bbbbbbbbbbbbbbbbbbbbbbbb', username: 'bbaker', fullName: 'Bob Baker', addresses: [{ street: '123 Front St.', city: 'Downtown', state: 'CA', zip: 12345 }] },
+      { _id: 'cccccccccccccccccccccccc', username: 'cconway', fullName: 'Chuck Conway' }
+    ]);
+
   }).then(results => {
     console.log('create new item: \n', results);
   })
@@ -56,18 +53,19 @@ mongoose.connect(
   // using .findByIdAndUpdate()
   // ***************************** 
   .then(() => {
-      return User
+    return User
       .findByIdAndUpdate(
         'aaaaaaaaaaaaaaaaaaaaaaaa',
-        { "$push": {
-            addresses:  {street: '789 Cactus St.', city: 'Scottsdale', state: 'AZ', zip: 56789} 
-          } 
-        }, 
-        {new: true} 
+        {
+          '$push': {
+            addresses: { street: '789 Cactus St.', city: 'Scottsdale', state: 'AZ', zip: 56789 }
+          }
+        },
+        { new: true }
       );
-        
+
   }).then(results => {
-    console.log('$push results: \n', results); 
+    console.log('$push results: \n', results);
   })
 
 
@@ -79,15 +77,13 @@ mongoose.connect(
   .then(() => {
     return User
       .findOneAndUpdate(
-        { username: 'aalabaster', 'addresses.state': 'AZ' }, 
-        { 'addresses.$.city': "Phoenix" },
-        {new: true}
+        { username: 'aalabaster', 'addresses.state': 'AZ' },
+        { 'addresses.$.city': 'Phoenix' },
+        { new: true }
       );
   }).then(results => {
-    console.log('update', results); 
+    console.log('update', results);
   })
-  
-
 
   // ***************************** 
   // $inc a property in an array
@@ -96,14 +92,13 @@ mongoose.connect(
   .then(() => {
     return User
       .findOneAndUpdate(
-        { username: 'aalabaster', 'addresses.state': 'AZ' }, 
+        { username: 'aalabaster', 'addresses.state': 'AZ' },
         { $inc: { 'addresses.$.likes': 1 } },
-        {new: true}
+        { new: true }
       );
   }).then(results => {
-    console.log('update', results); 
+    console.log('update', results);
   })
-
 
   // ***************************** 
   // $pull (remove) an item from an Array
@@ -113,10 +108,17 @@ mongoose.connect(
     return User
       .findByIdAndUpdate(
         'aaaaaaaaaaaaaaaaaaaaaaaa',
-        { $pull: { addresses: { city: 'Springfield'} } },
-        {new: true}
+        { $pull: { addresses: { city: 'Springfield' } } },
+        { new: true }
       );
   }).then(results => {
-    console.log('remove', results); 
+    console.log('remove', results);
+  })
+
+  .then(() => {
+
+    return mongoose.disconnect();
+  }).catch((err) => {
+    console.error(err);
+    return mongoose.disconnect();
   });
-  
